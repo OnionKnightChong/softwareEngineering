38294eeaa68cacaa56f63e522dac49292cd2c8ca
OnionKnightChong <chong.tian@bison.howard.edu>
2022-11-07 12:12:56 -0500

    demo.py

---
+++ demo.py
@@ -0,0 +1,21 @@
+import MySQLdb
+import tkinter
+
+db = MySQLdb.connect(host="grover.ctuetzgk1e5k.us-east-1.rds.amazonaws.com",    # host address
+		user="admin",         # username
+		passwd="howard123",  # password
+		db="grover")        # database name
+
+cur = db.cursor()
+
+print("-------------query users---------------")
+cur.execute("SELECT * FROM USER")
+for row in cur.fetchall():
+	print(row)
+
+print("-------------query categories---------------")
+cur.execute("SELECT * FROM CATEGORY") 
+for row in cur.fetchall():
+	print(row)
+
+db.close()
